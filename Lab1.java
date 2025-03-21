9de131130d6f6ca17159b749dda3cf9bfe3f48d1
Amarkumar-ranchi <amarkumar1234rock@gmail.com>
2025-03-21 18:17:09 +0530

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,24 @@
+package com.myjlc.loops;
+
+//1.Swap 2 Numbers
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+int a=10;
+int b=20;
+
+ System.out.println(a+"\t"+b);
+ 
+int temp=a;
+a=b;
+b=temp;
+
+System.out.println(a+"\t"+b); 
+}
+
+}
+
+//Time complexity-O(1)
+// Space complexity-O(1)
+// 1 is constant
